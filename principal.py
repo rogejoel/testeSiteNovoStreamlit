5762c2327b0d37e00e72e53a2c7b57bce9112a00
rogejoel <140846972+rogejoel@users.noreply.github.com>
2026-08-13 13:20:30 -0300

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Meu Super App", page_icon="🌐")
+
+st.title("Bem-vindo ao Meu Portal Interativo 🌐")
+st.write("Este site reúne várias ferramentas feitas com Python e Streamlit.")
+
+st.info("👈 Use o menu na barra lateral para navegar entre o Dashboard e o Chatbot!")
